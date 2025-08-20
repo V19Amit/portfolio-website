5f93f653655602d1075236c30b966539302fb268
Amit Verma <sumitverma2001v@gmail.com>
2025-08-20 14:25:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission (just alert for demo)
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Your message has been sent successfully!");
+});
