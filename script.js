f1f304fc94bb8ed8139da66f9bccae4616480b5b
Amit Verma <sumitverma2001v@gmail.com>
2026-08-18 23:13:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple form submission (just alert for demo)
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Your message has been sent successfully!");
-});
